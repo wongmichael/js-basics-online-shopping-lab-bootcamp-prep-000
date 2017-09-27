71c30c2a3fc819018e5054926e410d00d481ea01
Michael Wong <mik9080@gmail.com>
2017-09-27 16:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (!cart.length){
+    console.log("Your shopping cart is empty.")
+    return
+  }
+  var str = "In your cart, you have"
+  if (cart.length==1) {
+    return `${str} ${Object.keys(cart)[0]} at $${Object.values(cart)[0]}.`
+  } else if (cart.length==2) {
+      return `$(str) ${Object.keys(cart)[0]} at $${Object.values(cart)[0]} and ${Object.keys(cart)[1]} at $${Object.values(cart)[1]}.`
+  } else {
+      var str2
+      for (var i=0;i<cart.length-1;i++){
+        str2+=`${Object.keys(cart)[i]} at $${Object.values(cart)[i]},`
+      }
+      str2+=` and ${Object.keys(cart)[cart.length-1]} at $${Object.values(cart)[cart.length--1]}.`
+      return `${str} ${str2}`
+  }
 }
 
 function total() {
