1d9e2dde5107de1f11d04d1569d7e3f380879b5c
Michael Wong <mik9080@gmail.com>
2017-09-27 18:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,8 +56,22 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (var i=0;i<cart.length;i++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
+  return cart
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`)
+    cart=[]
+  }
 }
