b25d7d9de8b99af9a2b98320afbeef46b35757a9
Michael Wong <mik9080@gmail.com>
2017-09-27 16:05:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push({item:Math.floor(Math.random()*100)})
+ cart.push({[item]:Math.floor(Math.random()*100)})
  console.log(`${item} has been added to your cart.`)
  return cart
 }
