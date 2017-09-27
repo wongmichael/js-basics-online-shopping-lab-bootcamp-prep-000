859c1705eeeaf5ed7d8df7531e0e2e25d9f837cb
Michael Wong <mik9080@gmail.com>
2017-09-27 18:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var sum=0
+  for (var i=0;i<cart.length;i++){
+    sum+=cart[i][Object.keys(cart[i])]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
