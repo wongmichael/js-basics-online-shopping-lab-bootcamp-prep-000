9bbcf42e232c3d6d58ddeadf12d9f2f4dc1d571a
Michael Wong <mik9080@gmail.com>
2017-09-27 16:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ function viewCart() {
   } else if (cart.length==2) {
       return `$(str) ${Object.keys(cart)[0]} at $${Object.values(cart)[0]} and ${Object.keys(cart)[1]} at $${Object.values(cart)[1]}.`
   } else {
-      var str2=""
+      var str2
       for (var i=0;i<cart.length-1;i++){
         str2+=`${Object.keys(cart)[i]} at $${Object.values(cart)[i]},`
       }
-      str2=str2+` and ${Object.keys(cart)[cart.length-1]} at $${Object.values(cart)[cart.length--1]}.`
+      //str2=str2+
+      str2=`${str2} and ${Object.keys(cart)[cart.length-1]} at $${Object.values(cart)[cart.length--1]}.`
       return `${str} ${str2}`
   }
 }
