b99ae4b29fb84577043e80474977814a12d03cd2
Michael Wong <mik9080@gmail.com>
2017-09-27 16:50:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
       for (var i=0;i<cart.length-1;i++){
         str2+=`${Object.keys(cart)[i]} at $${Object.values(cart)[i]},`
       }
-      str2+=` and ${Object.keys(cart)[cart.length-1]} at $${Object.values(cart)[cart.length--1]}.`
+      str2=str2+` and ${Object.keys(cart)[cart.length-1]} at $${Object.values(cart)[cart.length--1]}.`
       return `${str} ${str2}`
   }
 }
